baa17da2a208fd0c18f108f19e9f3d792c7d6d71
pndesu <tranvanphucnguyen1@gmail.com>
2022-11-29 21:18:23 +1100

    Complete basic ui (title, positioning, website color palette) and range slider function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,51 @@
-const container = document.querySelector('.container');
-for (let i = 0; i < 16; i++){
-    let divRow = document.createElement('div');
-    divRow.style.display = 'flex';
-    for (let j = 0; j < 16; j++){
-        let div = document.createElement('div');
-        div.classList.add('gridBlock');
-        div.setAttribute('style', 'height: 39px; width: 39px; border: solid black 1px;');
-        divRow.appendChild(div);
-        div.addEventListener('mousedown', changeColor);
-        div.addEventListener('mouseover', changeColor);
+let slider = document.querySelector('.slider'); //Create slider and change slider value on mousemove
+let sliderValue = document.querySelectorAll('.sliderValue');
+let gridSize = slider.value;
+
+const container = document.querySelector('.container'); 
+function createGridBoard(gridSize){
+    for (let i = 0; i < gridSize; i++){
+        let divRow = document.createElement('div');
+        divRow.style.display = 'flex';
+        for (let j = 0; j < gridSize; j++){
+            let div = document.createElement('div');
+            div.setAttribute('style', `height: calc(600px/${gridSize}); width: calc(600px/${gridSize}); border: solid black 0.3px;`);
+            div.classList.add('gridBlock');
+            divRow.appendChild(div);
+            div.addEventListener('mousedown', changeColor);
+            div.addEventListener('mouseover', changeColor);
+        }
+        container.appendChild(divRow);
     }
-    container.appendChild(divRow);
+    resetButton();
 }
+function deleteGridBoard(){
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+  }
+}
+createGridBoard(gridSize);
+slider.addEventListener('input', function(e){
+    sliderValue.forEach(textValue => textValue.textContent = slider.value);
+    gridSize = slider.value;
+    deleteGridBoard();  
+    createGridBoard(gridSize);  
+})
 let mousedown = false;
-document.body.onmousedown = () => (mousedown = true);
-document.body.onmouseup = () => (mousedown = false);
+document.body.addEventListener("mousedown", () => (mousedown = true));
+document.body.addEventListener("mouseup", () => (mousedown = false));
 function changeColor(e){
-    console.log(mousedown);
+    // console.log(mousedown);
     if (e.type == 'mouseover' && !mousedown) return;
     e.target.classList.add('drawed');
 }
 
-const resetBtn = document.querySelector('.reset');
-const gridBoard = document.querySelectorAll('.gridBlock');
-resetBtn.addEventListener('click', function(e){
-    gridBoard.forEach(grid => grid.classList.remove('drawed'));
-})
+function resetButton(){
+    const resetBtn = document.querySelector('.reset');
+    const gridBoard = document.querySelectorAll('.gridBlock');
+    resetBtn.addEventListener('click', function(e){
+        gridBoard.forEach(grid => grid.classList.remove('drawed'));
+    })
+}
+resetButton();
+
