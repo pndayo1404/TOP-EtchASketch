82f035f8585d371d3a95ba2ecb912b4bf30dc2a0
pndesu <tranvanphucnguyen1@gmail.com>
2022-11-30 00:28:17 +1100

    Fix grid and reset buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,30 @@
-let slider = document.querySelector('.slider'); //Create slider and change slider value on mousemove
-let sliderValue = document.querySelectorAll('.sliderValue');
+const slider = document.querySelector('.slider'); //Create slider and change slider value on mousemove
+const sliderValue = document.querySelectorAll('.sliderValue');
 let gridSize = slider.value;
 let count = 0;
-
-const container = document.querySelector('.container'); //Create the first grid board
-createGridBoard(gridSize);
-slider.addEventListener('input', function(e){
-    sliderValue.forEach(textValue => textValue.textContent = slider.value);
-    gridSize = slider.value;
-    deleteGridBoard();  
-    createGridBoard(gridSize);  
-})
 
 let mousedown = false; //Logic to simulate mouse holding action
 document.body.addEventListener("mousedown", () => (mousedown = true));
 document.body.addEventListener("mouseup", () => (mousedown = false));
 
-
+const container = document.querySelector('.container'); //Create the first grid board
 const rainbowBtn = document.querySelector('.rainbow'); //Rainbow button
-rainbowBtn.addEventListener('click', toggleRainbow);
-
 const gridBtn = document.querySelector('.grid'); //Grid button
-gridBtn.addEventListener('click', toggleGrid);
-
 const resetBtn = document.querySelector('.reset'); //Reset button
-const gridBoard = document.querySelectorAll('.gridBlock');
-resetBtn.addEventListener('click', resetButton);
+
+
+createGridBoard(gridSize);
+let gridBoard = document.querySelectorAll('.gridBlock'); //Select all grid blocks
+slider.addEventListener('input', function(e){
+    sliderValue.forEach(textValue => textValue.textContent = slider.value);
+    gridSize = slider.value;
+    deleteGridBoard();  
+    gridBoard = createGridBoard(gridSize);  
+})
+
+rainbowBtn.addEventListener('click', toggleRainbow);
+gridBtn.addEventListener('click', toggleGrid);
+resetBtn.addEventListener('click', toggleReset);
 
 function createGridBoard(gridSize){ //Create grid board with range slider value
     for (let i = 0; i < gridSize; i++){
@@ -33,14 +32,22 @@ function createGridBoard(gridSize){ //Create grid board with range slider value
         divRow.style.display = 'flex';
         for (let j = 0; j < gridSize; j++){
             let div = document.createElement('div');
-            div.setAttribute('style', `height: calc(600px/${gridSize}); width: calc(600px/${gridSize}); border: solid black 0.3px;`);
+            div.setAttribute('style', `height: calc(600px/${gridSize}); width: calc(600px/${gridSize});`);
             div.classList.add('gridBlock');
+            if (gridBtn.classList.contains('gridOn')){
+                div.style.border = 'solid black 0.3px';
+            }
+            else{
+                div.style.border = 'none';
+            }
             divRow.appendChild(div);
             div.addEventListener('mousedown', changeColor);
             div.addEventListener('mouseover', changeColor);
         }
         container.appendChild(divRow);
     }
+    let gridBoard = document.querySelectorAll('.gridBlock');
+    return gridBoard;
 }
 
 function deleteGridBoard(){ //Delete grid board with function found on stackoverflow
@@ -54,9 +61,6 @@ function getColor(count, e){ //Color get 10% darker every time a grid is drawed
     let green = Math.ceil(Math.random()*256) - count*25.5; if (green < 0) green = 0;
     let blue = Math.ceil(Math.random()*256) - count*25.5; if (blue < 0) blue = 0;
     e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    console.log(red);
-    console.log(green);
-    console.log(blue);
 }
 function changeColor(e){
     if (e.type == 'mouseover' && !mousedown) return; //Logic to draw while mouse down
@@ -64,12 +68,11 @@ function changeColor(e){
         getColor(count, e);
         count += 1;
         if (count == 10) count = 0;
-        console.log(count);
     }
     else e.target.style.backgroundColor = 'black';
 }
 
-function resetButton(){ //Toggle reset button
+function toggleReset(){ //Toggle reset button
     gridBoard.forEach(grid => grid.style.backgroundColor = 'white');
     count = 0;
 }
@@ -84,11 +87,12 @@ function toggleRainbow(){ //Toggle rainbow button
 function toggleGrid(){ //Toggle grid button
     gridBtn.classList.toggle('gridOn');
     if (gridBtn.classList.contains('gridOn')){
-        gridBoard.forEach(gridBlock => gridBlock.style.border = 'solid black 0.3px');
+        gridBoard.forEach(grid => grid.style.border = 'solid black 0.3px');
         gridBtn.textContent = "Grid: On";
     }
     else{
-        gridBoard.forEach(gridBlock => gridBlock.style.border = 'none');
+        gridBoard.forEach(grid => grid.style.border = 'none');
         gridBtn.textContent = "Grid: Off";
     }
 }
+
